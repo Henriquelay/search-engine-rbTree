dafee613b3c8fe7e831c5eb87bee6932a7b781b9
icaro156 <icaro1561@outlook.com>
2021-04-28 22:04:48 -0300

    começo sem teste

---
+++ lib/data.h
@@ -0,0 +1,11 @@
+#ifndef DATA_H_
+#define DATA_H_
+#include<stdlib.h>
+#include<stdio.h>
+#include"./rbTree.h"
+
+void readData(char* filename, RBT* tree);
+int countLine(FILE* file);
+RBT* readPage(RBT* tree, char* pageName, char* filesouce, RBT* stopwords);
+RBT* readStops(RBT* tree,FILE* file);
+#endif // !DATA_H_#
