d5f8fcdcde2a0f29e113a500a04162b847fd0f5b
Jairo <jairo.moutinho@edu.ufes.br>
2021-05-09 02:55:10 -0300

    :adhesive_bandage: now <search> returns NULL if it searches for a word not in the symbol table

--- src/search.c
+++ src/search.c
@@ -40,44 +40,54 @@ RBT* get_RBT_interesction(RBT *smaller_tree, RBT* bigger_tree){
 
 
 RBT* start_intersection_tree(RBT* symbol_table, char* palavra){
-    RBT* page_tree = RBT_search(symbol_table, palavra)->value;
+    RBT* search_result = RBT_search(symbol_table, palavra);
+    if(!search_result){
+        return NULL;
+    }
+    RBT* page_tree = search_result->value;
     page_tree = get_RBT_interesction(page_tree, page_tree);
     return page_tree;
 }
 
 // Symbol table key: string, 
 // Symbol table val: RBT* (páginas [key: string, val: null])
-void search(char* line, RBT* symbol_table){
-    if(!line){return;}
+
+RBT* search(char* line, RBT* symbol_table){
+    if(!line){return NULL;}
     const char delim[2] = " ";
     char* search_keyword = strtok(line, delim);
     RBT* page_intersection_tree = NULL;
-    while(search_keyword != NULL && RBT_search(symbol_table,search_keyword) == NULL){
-        search_keyword = strtok(NULL, delim);
-    }
+
     if(search_keyword != NULL){
         page_intersection_tree = start_intersection_tree(symbol_table, search_keyword);
+        if(!page_intersection_tree){
+            return NULL;
+        }
         search_keyword = strtok(NULL, delim);
     }
 
-    while(search_keyword != NULL && RBT_search(symbol_table, search_keyword) != NULL){
-        RBT* page_tree = RBT_search(symbol_table, search_keyword)->value;
+    while(search_keyword != NULL){
+        RBT* search_result = RBT_search(symbol_table, search_keyword);
+        if(!search_result){
+            RBT_destroy(page_intersection_tree);
+            return NULL;
+        }
+        RBT* page_tree = search_result->value;
         RBT* page_tree_aux = get_RBT_interesction(page_intersection_tree, page_tree);
         RBT_destroy(page_intersection_tree);
+        if(!page_tree_aux){
+            return NULL;
+        }
         page_intersection_tree = page_tree_aux;
 
         search_keyword = strtok(NULL, delim);
     }
-    if(page_intersection_tree != NULL){
-    printf("#### DEBUG ####\n");
-    RBT_runOnAll_inOrder(page_intersection_tree, printa_key_RBT);
-    RBT_destroy(page_intersection_tree);
-    }
+    return page_intersection_tree;
 
 }
 
 
-void print_output(RBT* symbol_table){
+void print_output(RBT* symbol_table, RBT* page_graph){
     char* lineptr = NULL;
     size_t line_buffer_size = 0;
     ssize_t nCharacterRead;
@@ -88,7 +98,18 @@ void print_output(RBT* symbol_table){
         }
         if(!(*lineptr)){continue;}
         // puts(lineptr);
-        search(lineptr, symbol_table);
+        RBT* page_intersection_tree = search(lineptr, symbol_table);
+        if(page_intersection_tree){
+            RBT_runOnAll_inOrder(page_intersection_tree, printa_key_RBT);
+            int array_size = -1;
+            Page** page_array = RBT_value_to_array(page_intersection_tree, &array_size, page_graph);
+            printf("Funciona pf nunca te pedi nada\n");
+            printf("array_size: %d\n", array_size);
+            for(int i=0; i<array_size; i++){
+                printf("A página %d do array tem nome %s\n", i, page_array[i]->name);
+            }
+            RBT_destroy(page_intersection_tree);
+        }
     }
     free(lineptr);
 
